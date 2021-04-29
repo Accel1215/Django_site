ccc059b7428cba1c6a67741bbc741cb51ffd948d
Accel1215 <WeWillAlter@mail.ru>
2021-04-29 16:38:24 +0300

    Add polls and blogs apps.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
+    'blogs.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
